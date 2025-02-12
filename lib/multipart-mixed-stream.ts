8f6c811b2e5d09e2c256820dd403843a8e01d74d
Sven Rogge <sreglitzki@gmail.com>
2025-02-12 10:15:21 +0100

    Handle chunked parts

--- lib/multipart-mixed-stream.ts
+++ lib/multipart-mixed-stream.ts
@@ -8,10 +8,10 @@ const DLB = textEncoder.encode("\r\n\r\n");
 type IterableReadableStream<T> = ReadableStream<T> & AsyncIterable<T>;
 
 export function streamParts(
-  bodyOrContainer: Request | Response | ReadableStream<Uint8Array>,
+  bodyOrContainer: Request | Response | ReadableStream<Uint8Array | string>,
   boundary?: string
 ): IterableReadableStream<Part> {
-  let body: ReadableStream<Uint8Array> | undefined;
+  let body: ReadableStream<Uint8Array | string> | undefined;
   if (
     bodyOrContainer instanceof Request ||
     bodyOrContainer instanceof Response
@@ -24,22 +24,36 @@ export function streamParts(
     body = bodyOrContainer;
   }
   const b = textEncoder.encode(`--${boundary}\r\n`);
-  const end = textEncoder.encode(`\r\n--${boundary}--`);
+  const end = textEncoder.encode(`--${boundary}--`);
   let headers: Headers | undefined;
   let partWriter: WritableStreamDefaultWriter<Uint8Array> | undefined;
-  // TODO: Handle boundaries and headers potentially being split across chunks
-  const stream = new TransformStream<Uint8Array, Part>({
+  let margin: Uint8Array | undefined;
+
+  const stream = new TransformStream<Uint8Array | string, Part>({
     async transform(chunk, controller) {
       let bidx = 0;
+      if (typeof chunk === "string") chunk = textEncoder.encode(chunk);
+      if (margin) {
+        chunk = concat(margin, chunk);
+        margin = undefined;
+      }
+
       // Find all boundaries in current chunk
-      while ((bidx = findSubArray(chunk, b, bidx)) > -1) {
-        const headersEnd = findSubArray(chunk, DLB, bidx);
-        // If headers incomplete, don't process this chunk
-        if (headersEnd === -1) break;
-        if (bidx > 0 && partWriter) {
+      while ((bidx = indexOf(chunk, b)) > -1) {
+        const headersEnd = indexOf(chunk, DLB, bidx);
+        if (partWriter) {
           // Current part is complete
-          partWriter.write(chunk.slice(0, bidx - LB.length));
+          if (bidx > LB.length) {
+            // don't write the trailing line break
+            partWriter.write(chunk.slice(0, bidx - LB.length));
+          }
           partWriter.close();
+          partWriter = undefined;
+        }
+        if (headersEnd === -1) {
+          // If headers incomplete, don't process this chunk yet
+          margin = chunk;
+          return;
         }
         headers = parseHeaders(chunk.slice(bidx + b.length, headersEnd));
         const { readable, writable } = new TransformStream();
@@ -49,12 +63,21 @@ export function streamParts(
         bidx = 0;
       }
 
-      // No more boundaries in current chunk, write remaining data to part stream
-      if (chunk.length > 0) {
-        if (findSubArray(chunk, end, -end.length))
-          chunk = chunk.slice(0, -end.length);
-        partWriter?.write(chunk);
+      const endIdx = indexOf(chunk, end, -end.length);
+      if (endIdx > -1) {
+        // This is the last chunk / part, write without end boundary and line break
+        if (endIdx > LB.length) {
+          partWriter?.write(chunk.slice(0, endIdx - LB.length));
+        }
+        return;
       }
+      // Boundary could be split across chunks, so we keep the tail of the chunk
+      if (chunk.length >= b.length + LB.length) {
+        partWriter?.write(chunk.slice(0, -b.length - 1));
+        margin = chunk.slice(-b.length - 1);
+        return;
+      }
+      margin = chunk;
     },
     flush() {
       partWriter?.close();
@@ -63,7 +86,17 @@ export function streamParts(
   return body.pipeThrough(stream) as IterableReadableStream<Part>;
 }
 
-function findSubArray(data: Uint8Array, sub: Uint8Array, startAt = 0) {
+function concat(
+  chunk1: Uint8Array<ArrayBufferLike>,
+  chunk2: Uint8Array<ArrayBufferLike>
+) {
+  const arr = new Uint8Array(chunk1.length + chunk2.length);
+  arr.set(chunk1);
+  arr.set(chunk2, chunk1.length);
+  return arr;
+}
+
+function indexOf(data: Uint8Array, sub: Uint8Array, startAt = 0) {
   for (let i = startAt; i < data.length; i++) {
     for (let j = 0; j < sub.length; j++) {
       if (data[i + j] !== sub[j]) break;
